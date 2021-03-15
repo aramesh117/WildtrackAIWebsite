1aa393231bb1e24e65c50d5a8e1f0dd35c7e37a9
Jonathan D'Souza <jtdsouza@gmail.com>
2021-03-15 08:22:12 -0400

    updates to save data

--- app.py
+++ app.py
@@ -1730,3 +1730,16 @@ def delete_user():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
+
+@app.route('/add_observation', methods=['POST'])   
+
+def add_observation():
+    #try:
+    data=request.values
+    files=request.files.getlist('images')
+    result=Create_Observation(data,files)
+    print(data)
+    return json.dumps({'status':resullt})
+
+
+   
